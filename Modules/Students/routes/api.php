000811f371088095c839ca14199a2be73c4e3e90
kz370 <kz.dev370@gmail.com>
2026-01-12 11:42:39 +0000

    Fix API route prefixes: Remove redundant 'api/' from module routes

--- Modules/Students/routes/api.php
+++ Modules/Students/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Students\Http\Controllers\AuthController;
 use Modules\Students\Http\Controllers\StudentsController;
 
-Route::middleware(['api'])->prefix('api/students')->name('api.students.')->group(function () {
+Route::middleware(['api'])->prefix('students')->name('api.students.')->group(function () {
     // Define your module API routes here
     // Example:
     // Route::apiResource('students', StudentsController::class);
